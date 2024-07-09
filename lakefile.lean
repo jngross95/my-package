9056ee634193d099fc2a66f37ddd83836c487976
Jürgen Gross <jngross95@gmail.com>
2024-07-09 08:07:22 +0200

    ui

--- lakefile.lean
+++ lakefile.lean
@@ -1,11 +1,17 @@
 import Lake
 open Lake DSL
 
-package «my-package» where
+package «my-package»  {
   -- Settings applied to both builds and interactive editing
   leanOptions := #[
     ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
   ]
+
+  moreLinkArgs := #[
+    "-L./.lake/packages/LeanCopilot/.lake/build/lib",
+    "-lctranslate2"
+  ]
+  }
   -- add any additional package configuration options here
 
 require mathlib from git
@@ -20,3 +26,5 @@ require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
 meta if get_config? env = some "dev" then
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
 
+
+require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "v1.4.0"
