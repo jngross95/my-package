197fa79245d4c767bdd7b64ffe1b01b1061da093
Jürgen Gross <jngross95@gmail.com>
2024-07-10 21:46:00 +0200

    öä

--- lakefile.lean
+++ lakefile.lean
@@ -31,5 +31,3 @@ require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
 --require LeanTeX from git "https://github.com/kmill/LeanTeX.git" @ "main"
 
 meta if get_config? env = some "dev" then
-require «doc-gen4» from git
-  "https://github.com/leanprover/doc-gen4" @ "main"
