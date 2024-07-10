34282bfd1da894572feb1f285ba515996498a3b5
Jürgen Gross <jngross95@gmail.com>
2024-07-10 21:39:37 +0200

    Setup blueprint

--- lakefile.lean
+++ lakefile.lean
@@ -29,3 +29,9 @@ require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
 
 --require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "v1.4.0"
 --require LeanTeX from git "https://github.com/kmill/LeanTeX.git" @ "main"
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
