8518b62d4e150501b54a5f76f0dafd9fe1c72fdf
Jürgen Gross <jngross95@gmail.com>
2024-07-15 12:20:02 +0200

    examples

--- MyPackage/Basic.lean
+++ MyPackage/Basic.lean
@@ -85,3 +85,82 @@ example (h : C ∨ O) : O ∨ C := by
   | inr ho => -- Fall O
     left
     exact ho
+
+
+
+
+--example (G H U : Prop)(h : G ∨ H ∧ U) : (G ∨ H) ∧ (G ∨ U) := by
+--   have h2: (G ∨ (H ∧ U)) := h
+
+
+---- NOT NOT p
+example (P : Prop)(p : P) : ¬¬P := by
+have f: (P->False) -> False :=
+  λ f:P->False => (f p)
+exact f
+
+--  ((p->false) → false)
+example (L : Prop) : ¬(L ∧ ¬L) := by
+have x:  (L ∧ (L -> False)) -> False :=
+  λ h => (h.right h.left)
+exact x
+
+-----------
+
+example (B S : Prop)(h1 : B → S)(h2 : S->False) : B->False := by
+exact λ b:B => h2 (h1 b)
+
+-----------
+
+example (A : Prop) (h: A → (A->False)) : A->False := by
+exact λ a:A => (h a) a
+
+-----------
+
+example (B C : Prop) (h: (B → C)->False) : C->False := by
+exact λ c:C => (h λ _:B=>c)
+
+-----------
+
+example (C S : Prop) (s: S) : ((S->False) ∧ C)->False := by
+exact λ and_term:((S->False) ∧ C) =>   (and_term.left s)
+
+-------
+
+example (A P : Prop) (h : P → (A->False)) : (P ∧ A)->False := by
+exact λ pa: P ∧ A => ((h pa.left) pa.right)
+
+---------
+
+example (A P : Prop) (h: (P ∧ A) → False) : P → (A → False) := by
+exact λ (p:P) (a:A) => h ⟨p , a⟩
+
+--------
+--example (A : Prop)(h : ((A->False)->False)->False) : A -> False := by
+--exact λ a:A => (h λ         )
+
+--example (A : Prop)(h : ¬¬¬A) : ¬A := by
+--let thm (P : Prop)(p : P) : ¬¬P :=
+--  let f: (P->False) -> False :=  λ f:P->False => (f p)
+--  f
+
+---
+example {A : Prop} (h : ¬¬¬A) : ¬A := by
+  exact fun a : A => h (fun na : ¬A => na a)
+
+--
+example (B C : Prop) (h : ¬(B → C)) : ¬¬B := by
+ exact   fun (nb : ¬ B) =>
+    h (fun (b : B) => False.elim (nb b))
+
+
+example (B C : Prop) (h : ¬(B → C)) : ¬¬B := by
+intro
+apply h
+intro
+contradiction
+
+--example (B P : Prop) (h : B ↔ ¬P) : (B → ¬P) ∧ (¬P → B) := by
+--have x :=  iff_mp h
+--have y :=  iff_mpr h
+--exact And.intro x y
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # my-package
 
+https://github.com/ImperialCollegeLondon/M40001_lean/tree/master/src
+
 
 python -m pip install --config-settings="--global-option=build_ext" `
                       --config-settings="--global-option=-IC:\Program Files\Graphviz\include" `
@@ -80,3 +82,67 @@ example (B C I : Prop)(h1 : C → B)(h2 : I → B)(h3 : C ∨ I) : B := by
 have hh := or_elim h3 h1 h2
 exact hh
 ---------------
+example (C O : Prop)(h : C ∨ O) : O ∨ C := by
+have x: C → O ∨ C := or_inr
+have y: O → O ∨ C := or_inl
+exact or_elim h x y 
+-----------
+example (C J R : Prop)(h1 : C → J)(h2 : C ∨ R) : J ∨ R := by
+have x: J → J ∨ R := or_inl
+have y: R → J ∨ R := or_inr
+exact or_elim h2 (λc=> x (h1 c)) (λr=> y r) 
+
+----
+example (G H U : Prop)(h : G ∧ (H ∨ U)) : (G ∧ H) ∨ (G ∧ U) := by
+have g := h.left
+
+
+have f1:(H->(G ∧ H) ∨ (G ∧ U)) := λh:H => (or_inl (and_intro g h))
+
+have f2:(U->(G ∧ H) ∨ (G ∧ U)) := λu:U => (or_inr (and_intro g u))
+
+have z := or_elim h.right  f1 f2
+exact z
+---------
+example (I K P : Prop)(h : K → P) : K ∨ I → I ∨ P := by
+exact λ h2:(K ∨ I) => 
+ 
+   or_elim h2 (λ k:K => or_inr (h k))  (λ i:I => or_inl i)
+
+-----------------------
+example (P Q R : Prop)(h : P ∨ Q ∧ R) : (P ∨ Q) ∧ (P ∨ R) := by
+constructor
+cases h
+left
+assumption
+right
+cases h_1
+assumption
+cases h
+left 
+assumption
+cases h_1
+right
+assumption
+----------------
+example (P Q R : Prop)(h : P ∧ (Q ∨ R)) : (P ∧ Q) ∨ (P ∧ R) := by
+cases h
+cases right
+left
+constructor
+assumption
+assumption
+right
+constructor
+assumption
+assumption
+
+----------------
+example (P Q R : Prop)(h : Q → R) : Q ∨ P → P ∨ R := by
+
+intro
+cases a
+right
+apply (h h_1)
+left
+apply h_1
